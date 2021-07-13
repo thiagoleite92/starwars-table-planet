8a76579cb350d7254ea4a443ffdce26a1472bb3e
Thiago Leite <leite.tjs@gmail.com>
2021-07-13 13:06:25 -0300

    fixing deploy

---
+++ build/precache-manifest.7c432ec045f4767afce760d8af4e8127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39d0a608419fd7e675a7558819b922e",
+    "url": "/starwars-table-planets/index.html"
+  },
+  {
+    "revision": "6e09540743e32c28e9f6",
+    "url": "/starwars-table-planets/static/css/main.31fff509.chunk.css"
+  },
+  {
+    "revision": "4d381a955343f82e4e11",
+    "url": "/starwars-table-planets/static/js/2.6bc20a99.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-table-planets/static/js/2.6bc20a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e09540743e32c28e9f6",
+    "url": "/starwars-table-planets/static/js/main.e2b03680.chunk.js"
+  },
+  {
+    "revision": "42a67ce262b1eb8ad95f",
+    "url": "/starwars-table-planets/static/js/runtime-main.a55f068f.js"
+  }
+]);
